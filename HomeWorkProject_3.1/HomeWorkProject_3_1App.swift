ecc8a583bb1178d469f8092a8fa37d406e9e9c54
Maxim Goryachkin <maxgoryachkin@mail.ru>
2021-09-07 21:18:54 +0300

    Initial Commit

---
+++ HomeWorkProject_3.1/HomeWorkProject_3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWorkProject_3_1App.swift
+//  HomeWorkProject_3.1
+//
+//  Created by Максим on 07.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct HomeWorkProject_3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
